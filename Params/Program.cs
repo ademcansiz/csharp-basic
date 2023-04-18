0efca979f0e29b36b1d3afa6521d08cab6a79f66
ademcnsz <ademmcansiz@gmail.com>
2023-04-18 09:21:10 +0300

    params

---
+++ Params/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            var toplam =  user.Calc(4,9,82,56);
+            Console.WriteLine("Sayıların toplamı : " + toplam);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Params/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    public class User
+    {  
+        public int Calc(params int[] numbers) 
+        {
+            int result=0;
+            foreach (var item in numbers)
+            {
+                result += item;
+            }
+            return result;
+        }
+    }
+}
