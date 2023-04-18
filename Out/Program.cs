8ac15ce39c2c750f09c7bf51389643c5b7978747
ademcnsz <ademmcansiz@gmail.com>
2023-04-18 10:15:54 +0300

    Properties

---
+++ Out/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            int age;
+            user.SetAge(out age);
+            Console.WriteLine(age);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Out/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out
+{
+    public class User
+    {
+        public void SetAge(out int age)
+        {
+            age = 10;
+        }
+    }
+}
